060b411c62eeeea02c0303dd3e5b14906fb3475c
Nathan Stilwell <nathanstilwell@gmail.com>
2012-11-04 17:13:12 -0500

    filtering noise

--- js/app.js
+++ js/app.js
@@ -1,8 +1,7 @@
 define([
   'jquery',
-  'src/countdown',
-  'src/data'
-], function ($, countdown, data) {
-  countdown.init($('#clock'), data.launchDate);
+  'src/countdown'
+], function ($, countdown) {
+  countdown.init($('#clock'), "November 6, 2012 00:00:00");
 });
 
